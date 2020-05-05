d2eb3f42b0f7b48a450c0781a957be6b73450ca6
SuperCoder7979 <k.pranav@gmail.com>
2020-05-04 22:42:13 -0400

    fixes

--- src/main/java/supercoder79/cavebiomes/cave/CaveDecorators.java
+++ src/main/java/supercoder79/cavebiomes/cave/CaveDecorators.java
@@ -37,7 +37,7 @@ public class CaveDecorators {
     public static final CaveDecorator GOLD = new RandomBlockStateCaveDecorator(Blocks.GOLD_ORE.getDefaultState(), 24);
     public static final CaveDecorator REDSTONE = new RandomBlockStateCaveDecorator(Blocks.REDSTONE_ORE.getDefaultState(), 12);
     public static final CaveDecorator LAPIS = new RandomBlockStateCaveDecorator(Blocks.LAPIS_ORE.getDefaultState(), 16);
-    public static final CaveDecorator DIAMOND = new RandomBlockStateCaveDecorator(Blocks.DIAMOND_ORE.getDefaultState(), 48);
+    public static final CaveDecorator DIAMOND = new RandomBlockStateCaveDecorator(Blocks.DIAMOND_ORE.getDefaultState(), 128);
 
     //TODO: big mushrooms
     //TODO: honey cave
