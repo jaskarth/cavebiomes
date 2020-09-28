d1ff97fb9295fc0b1ca67364dac43d1bb61b2f67
SuperCoder7979 <k.pranav@gmail.com>
2020-09-28 08:47:28 -0400

    fix bugs

--- src/main/java/supercoder79/cavebiomes/CaveBiomes.java
+++ src/main/java/supercoder79/cavebiomes/CaveBiomes.java
@@ -18,7 +18,7 @@ import java.util.Map;
 import java.util.Optional;
 
 public class CaveBiomes implements ModInitializer {
-	public static final String VERSION = "0.3.1";
+	public static final String VERSION = "0.3.2";
 
 	public static ConfigData CONFIG;
 
